925b7d92c486d6a6ff8703b9dea524a253525f37
joyboy16 <romiboy16@gmail.com>
2017-01-18 01:59:57 +0100

    Tutorial 7
    tutorial by Thinmatrix

---
+++ src/engineTester/MainGameLoop.java
@@ -0,0 +1,66 @@
+package engineTester;
+ 
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.GL11;
+import org.lwjgl.util.vector.Vector3f;
+import org.newdawn.slick.opengl.LoadableImageData;
+
+import entities.Entity;
+import models.RawModel;
+import models.TexturedModel;
+import renderEngine.DisplayManager;
+import renderEngine.Loader;
+import renderEngine.Renderer;
+import shaders.StaticShader;
+import textures.ModelTexture;
+ 
+public class MainGameLoop {
+ 
+    public static void main(String[] args) {
+ 
+        DisplayManager.createDisplay();
+        Loader loader = new Loader();
+        Renderer renderer = new Renderer();
+        StaticShader shader = new StaticShader();
+         
+        float[] vertices = {            
+                -0.5f,0.5f,0,   //V0
+                -0.5f,-0.5f,0,  //V1
+                0.5f,-0.5f,0,   //V2
+                0.5f,0.5f,0     //V3
+        };
+         
+        int[] indices = {
+                0,1,3,  //Top left triangle (V0,V1,V3)
+                3,1,2   //Bottom right triangle (V3,V1,V2)
+        };
+        
+        float[] textureCoords = {
+        		0,0,	//V0
+        		0,1,	//V1
+        		1,1,	//V2
+        		1,0		//V3
+        };
+        RawModel model = loader.loadToVAO(vertices,textureCoords, indices);
+        
+        TexturedModel staticModel = new TexturedModel(model, new ModelTexture(loader.loadTexture("texture")));
+        
+      //  Entity entity = new Entity(staticModel, new Vector3f(-1, 0, 0), 0, 0, 0, 1);
+        
+        while(!Display.isCloseRequested()){
+          //  entity.increasePosition(0.002f,0, 0);
+          //  entity.increaseRotation(0, 1, 0);
+            renderer.prepare();
+            shader.start();
+            renderer.render(staticModel);
+            shader.stop();
+            DisplayManager.updateDisplay();         
+        }
+ 
+        shader.cleanUp();
+        loader.cleanUp();
+        DisplayManager.closeDisplay();
+ 
+    }
+ 
+}
