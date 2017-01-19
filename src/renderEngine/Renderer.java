ee52a3d44d5b6312d272369a49549ee3e871af0f
Diganta Barai <diganta.barai@rwth-aachen.de>
2017-01-19 16:16:12 +0100

    Tutorial 8

--- src/renderEngine/Renderer.java
+++ src/renderEngine/Renderer.java
@@ -17,45 +17,44 @@ import entities.Entity;
  
 public class Renderer {
      
-  //  private static final float FOV = 70;
-  //  private static final float NEAR_PLANE = 0.1f;
-  //  private static final float FAR_PLANE = 1000;
+    private static final float FOV = 70;
+    private static final float NEAR_PLANE = 0.1f;
+    private static final float FAR_PLANE = 1000;
      
     private Matrix4f projectionMatrix;
      
-  //  public Renderer(StaticShader shader){
-       // createProjectionMatrix();
-      //  shader.start();
-       // shader.loadProjectionMatrix(projectionMatrix);
-        //shader.stop();
-   // }
+    public Renderer(StaticShader shader){
+        createProjectionMatrix();
+        shader.start();
+        shader.loadProjectionMatrix(projectionMatrix);
+        shader.stop();
+    }
  
     public void prepare() {
-      //  GL11.glEnable(GL11.GL_DEPTH_TEST);
-        GL11.glClear(GL11.GL_COLOR_BUFFER_BIT);
+        GL11.glEnable(GL11.GL_DEPTH_TEST);
+        GL11.glClear(GL11.GL_COLOR_BUFFER_BIT|GL11.GL_DEPTH_BUFFER_BIT);
         GL11.glClearColor(1,0,0,1);
     }
  
-	public void render(TexturedModel texturedmodel) {
+	public void render(Entity entity, StaticShader shader) {
 		// TODO Auto-generated method stub
-	      //TexturedModel model = entity.getModel();
-	        RawModel rawModel = texturedmodel.getRawModel();
+	      	TexturedModel model = entity.getModel();
+	        RawModel rawModel = model.getRawModel();
 	        GL30.glBindVertexArray(rawModel.getVaoID());
 	        GL20.glEnableVertexAttribArray(0);
-	       // GL11.glDrawArrays(GL11.GL_TRIANGLES, 0, rawModel.getVertexCount());
 	        GL20.glEnableVertexAttribArray(1);
-	      //  Matrix4f transformationMatrix = Maths.createTransformationMatrix(entity.getPosition(),
-	        //       entity.getRotX(), entity.getRotY(), entity.getRotZ(), entity.getScale());
-	        //shader.loadTransformationMatrix(transformationMatrix);
+	        Matrix4f transformationMatrix = Maths.createTransformationMatrix(entity.getPosition(),
+	               entity.getRotX(), entity.getRotY(), entity.getRotZ(), entity.getScale());
+	        shader.loadTransformationMatrix(transformationMatrix);
 	        GL13.glActiveTexture(GL13.GL_TEXTURE0);
-	        GL11.glBindTexture(GL11.GL_TEXTURE_2D, texturedmodel.getTexture().getID());
+	        GL11.glBindTexture(GL11.GL_TEXTURE_2D, model.getTexture().getID());
 	        GL11.glDrawElements(GL11.GL_TRIANGLES, rawModel.getVertexCount(), GL11.GL_UNSIGNED_INT, 0);
 	        GL20.glDisableVertexAttribArray(0);
 	        GL20.glDisableVertexAttribArray(1);
 	        GL30.glBindVertexArray(0);
 	}
      
-  /*  private void createProjectionMatrix(){
+    private void createProjectionMatrix(){
         float aspectRatio = (float) Display.getWidth() / (float) Display.getHeight();
         float y_scale = (float) ((1f / Math.tan(Math.toRadians(FOV / 2f))) * aspectRatio);
         float x_scale = y_scale / aspectRatio;
@@ -69,5 +68,5 @@ public class Renderer {
         projectionMatrix.m32 = -((2 * NEAR_PLANE * FAR_PLANE) / frustum_length);
         projectionMatrix.m33 = 0;
     }
- */
+ 
 }
