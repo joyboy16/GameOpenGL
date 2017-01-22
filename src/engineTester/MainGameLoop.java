6692345d9255bdc436d2ee4250a64a03bf2600ef
Diganta Barai <diganta.barai@rwth-aachen.de>
2017-01-22 20:03:55 +0100

    Tutorial 15

--- src/engineTester/MainGameLoop.java
+++ src/engineTester/MainGameLoop.java
@@ -9,6 +9,7 @@ import models.TexturedModel;
 
 import org.lwjgl.opengl.Display;
 import org.lwjgl.util.vector.Vector3f;
+import org.newdawn.slick.tests.TestUtils;
 
 import renderEngine.DisplayManager;
 import renderEngine.Loader;
@@ -31,18 +32,31 @@ public class MainGameLoop {
 		RawModel model = OBJLoader.loadObjModel("tree", loader);
 		
 		TexturedModel staticModel = new TexturedModel(model,new ModelTexture(loader.loadTexture("tree")));
+		TexturedModel grass = new TexturedModel(OBJLoader.loadObjModel("grassModel", loader), 
+				new ModelTexture(loader.loadTexture("grassTexture")));
+		grass.getTexture().setHasTransparency(true);
+		grass.getTexture().setUseFakeLighting(true);
+		TexturedModel fern = new TexturedModel(OBJLoader.loadObjModel("fern", loader),
+				new ModelTexture(loader.loadTexture("fern")));
+		fern.getTexture().setHasTransparency(true);
+		//fern.getTexture().setUseFakeLighting(true);
 		
 		List<Entity> entities = new ArrayList<Entity>();
 		Random random = new Random();
 		for(int i=0;i<500;i++){
-			entities.add(new Entity(staticModel, new Vector3f(random.nextFloat()*800 - 400,0,random.nextFloat() * -600),0,0,0,3));
+			entities.add(new Entity(staticModel, new Vector3f(random.nextFloat()*800 - 400,0,
+					random.nextFloat() * -600),0,0,0,3));
+			entities.add(new Entity(grass, new Vector3f(random.nextFloat()*800 - 400,0,
+					random.nextFloat() * -600),0,0,0,1));
+			entities.add(new Entity(fern, new Vector3f(random.nextFloat()*800 - 400,0,
+					random.nextFloat() * -600),0,0,0,0.6f));
 		}
 		
 		Light light = new Light(new Vector3f(20000,20000,2000),new Vector3f(1,1,1));
 		
 		Terrain terrain = new Terrain(0,0,loader,new ModelTexture(loader.loadTexture("grass")));
 		Terrain terrain2 = new Terrain(1,0,loader,new ModelTexture(loader.loadTexture("grass")));
-		Terrain terrain3 = new Terrain(0,1,loader,new ModelTexture(loader.loadTexture("grass")));
+		
 		
 		Camera camera = new Camera();	
 		MasterRenderer renderer = new MasterRenderer();
@@ -52,9 +66,8 @@ public class MainGameLoop {
 			
 			renderer.processTerrain(terrain);
 			renderer.processTerrain(terrain2);
-			renderer.processTerrain(terrain3	);
 			for(Entity entity:entities){
-				//renderer.processEntity(entity);
+				renderer.processEntity(entity);
 			}
 			renderer.render(light, camera);
 			DisplayManager.updateDisplay();
